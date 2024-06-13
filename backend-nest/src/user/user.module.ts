62cb472565282a8c872f3d96313c2c8f5b2e4dcc
yongtaecheon <ide2143@naver.com>
2024-06-13 21:59:12 +0900

    nest userModule TypeOrm import 추가

--- backend-nest/src/user/user.module.ts
+++ backend-nest/src/user/user.module.ts
@@ -3,8 +3,21 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TokenService } from 'src/auth/token.service';
 import { UserRepository } from './user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/auth/entities/user.entity';
+import { CatInfoEntity } from 'src/auth/entities/catInfo.entity';
+import { ChatInfoEntity } from './entities/chatInfo.entity';
+import { SurveyInfoEntity } from './entities/surveyInfo.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      CatInfoEntity,
+      ChatInfoEntity,
+      SurveyInfoEntity,
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, TokenService, UserRepository],
 })
